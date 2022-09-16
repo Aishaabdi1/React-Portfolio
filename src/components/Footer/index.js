2a4ed8068c12e15bf92e0dc7ec610f5d14fa70b5
Aishaabdi1 <aishahajisirad@gmail.com>
2022-09-16 01:30:51 +0100

    linkedin and twitter page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,6 +15,30 @@ function Footer() {
 						className="logo"
 					></img>
 				</a>
+                <a
+					href="https://www.linkedin.com/in/aisha-abdi-30674423a/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/linkedin-logo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://twitter.com/AishaAb19440194"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/twitter-logo.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
 			</div>
 		</footer>
 	);
